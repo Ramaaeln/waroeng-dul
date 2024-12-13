d51d2261959f6df6f64d3e784aee6aa10454f39b
codewithramaa <abdullahramadanelansary@gmail.com>
2024-12-13 16:37:56 +0700

    commit url

--- fe/src/components/Elements/Card/index.jsx
+++ fe/src/components/Elements/Card/index.jsx
@@ -7,7 +7,7 @@ const Card = ({ search, addToCart}) => {
 
   useEffect(() => {
     axios
-      .get('https://candy-cute-zebu.glitch.me/menu')
+      .get('https://second-thankful-drifter.glitch.me/menu')
       .then((response) => {
         setMenu(response.data);
       })
