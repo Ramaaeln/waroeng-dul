b86dbf62d51bde89801883b0f5e601f139f3dbb7
Ramaaeln <abdullahramadanelansary@gmail.com>
2025-01-07 14:42:28 +0700

    update

--- be/connection.js
+++ be/connection.js
@@ -1,15 +1,16 @@
+require('dotenv').config();  // Memuat variabel lingkungan dari .env
+
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: "aws-0-ap-southeast-1.pooler.supabase.com",
-  user: "postgres.axxjitvnhqfxavdndbdd",
-  database: "postgres",
-  password: "hLjyGAd7iX0ZKHEm",
-  port: "6543",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
   max: 20,
   idleTimeoutMillis: 2000,
   connectionTimeoutMillis: 2000,
 });
 
 module.exports = pool;
-
